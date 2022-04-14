296057a341ca6fa3491d38e2e89c9daef4c4116b
ThreeEyedGod <venkatesh.narayanan@live.in>
2022-04-14 18:26:18 +0530

    Update Embedded.hs

--- src/Data/Aeson/Embedded.hs
+++ src/Data/Aeson/Embedded.hs
@@ -19,10 +19,11 @@ import qualified Data.Attoparsec.Text as AText
 -- | Type for a JSON value embedded within a JSON string value
 newtype Embedded a = Embedded { _unEmbed :: a } deriving (Eq, Show)
 
-instance FromJSON a =>
+{-- instance FromJSON a =>
          FromText (Embedded a) where
   parser =
     fmap Embedded . either fail pure . eitherDecodeStrict . encodeUtf8 =<< AText.takeText
+    --}
 
 instance FromJSON a =>
          FromJSON (Embedded a) where
