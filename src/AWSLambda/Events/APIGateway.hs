2c9b1f0e9ee86f3e1d169211fe63d9df3a410f50
ThreeEyedGod <venkatesh.narayanan@live.in>
2022-04-17 18:06:33 +0530

    Update APIGateway.hs

--- src/AWSLambda/Events/APIGateway.hs
+++ src/AWSLambda/Events/APIGateway.hs
@@ -98,7 +98,7 @@ $(makeLenses ''RequestIdentity)
 data Authorizer = Authorizer
   { _aPrincipalId :: !(Maybe Text)
   , _aClaims :: !Object
-  , _aContext :: !HashMap (Maybe Text) Object
+  , _aContext :: HashMap (Maybe Text) Object
   } deriving (Eq, Show)
   
 instance FromJSON Authorizer where
